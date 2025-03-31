e308cbcd050824a7b309f2c7095a3981e01eb297
iagugunava <iagugunava05@gmail.com>
2025-03-31 15:51:34 +0400

    merge

--- components/Projects.vue
+++ components/Projects.vue
@@ -1,13 +1,65 @@
 <script setup lang="ts">
+const supabaseClient = useSupabaseClient();
 
+const containerRef = ref(null);
+const swiper = useSwiper(containerRef, {
+  effect: "creative",
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  creativeEffect: {
+    prev: {
+      shadow: true,
+      translate: [0, 0, -500],
+    },
+    next: {
+      shadow: true,
+      translate: [0, 0, -500],
+    },
+  },
+});
+
+onMounted(() => {
+  console.log(swiper.instance);
+});
+
+const projects = ref();
+
+const fetchProjects = async () => {
+  const { data, error } = await supabaseClient.from("projects").select("*");
+
+  if (!error) {
+    projects.value = data;
+  } else {
+    console.log(error);
+  }
+
+  console.log(projects.value);
+};
+
+fetchProjects();
 </script>
 
 <template>
-    <div>
-        projects
+  <div class="py-20">
+    <div class="container-fluid">
+        <div class="mb-8">
+          <h2 class="font-semibold text-4xl text-dark w-fit">My Projects</h2>
+        </div>
+        <div>
+          <swiper-container
+            class="!overflow-visible"
+            ref="containerRef"
+            :init="false"
+          >
+            <swiper-slide v-for="item in projects" :key="item?.id">
+              <Card :data="item" />
+            </swiper-slide>
+          </swiper-container>
+        </div>
     </div>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ components/ui/Card.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const props = defineProps<{
+  data: any;
+}>();
+</script>
+
+<template>
+  <NuxtLink :to="data?.link" target="_blank" class="group">
+    <div class="relative w-full overflow-hidden rounded-lg">
+        <NuxtImg
+          :src="data?.image"
+          :alt="data?.name"
+          class="aspect-[351/216] w-full object-cover transition-all duration-500 ease-in-out lg:aspect-[542/300] 2xl:aspect-[778/456]"
+        />
+    </div>
+    <h3>{{ data?.name }}</h3>
+  </NuxtLink>
+</template>
+
+<style scoped></style>
