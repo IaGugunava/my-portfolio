8315d279c641f072cb02d64de751905e5df62083
iagugunava <iagugunava05@gmail.com>
2025-04-12 16:28:40 +0400

    merge

--- components/layout/footer/TheFooter.vue
+++ components/layout/footer/TheFooter.vue
@@ -1,13 +1,18 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-    <div>
-        footer
+  <div class="mt-20 bg-dark mx-auto py-20">
+    <div class="container-fluid flex justify-center items-center">
+      <h2 class="text-white max-w-[800px] text-center text-xl">
+        What’s next? Feel free to reach out to me if you're looking for a
+        developer, have a query, or simply want to connect.
+      </h2>
+
+      <div class="flex gap-2 items-center">
+        
+      </div>
     </div>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
