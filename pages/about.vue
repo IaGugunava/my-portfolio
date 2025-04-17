26219a2eb5920aee09bbee3f99d96942d66d7a33
iagugunava <iagugunava05@gmail.com>
2025-04-17 18:27:13 +0400

    wip: about components

--- pages/about.vue
+++ pages/about.vue
@@ -1,13 +1,25 @@
 <script setup lang="ts">
+const supabaseClient = useSupabaseClient();
 
+const imgSource = ref("");
+
+const getImage = async () => {
+  const { data } = await supabaseClient.storage
+    .from("avatars")
+    .getPublicUrl("about-profile.jpeg");
+
+  imgSource.value = data?.publicUrl;
+};
+
+onMounted(() => {
+    getImage()
+})
 </script>
 
 <template>
-    <div>
-        about me
-    </div>
+  <div class="py-20">
+    <AboutBanner :image="imgSource"/>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
