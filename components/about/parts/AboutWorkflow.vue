df5cfe6ba99a55ce50e928544f5500359d8b777c
ia <iagugunava05@gmail.com>
2025-05-05 02:01:27 +0400

    wip: about workflow

---
+++ components/about/parts/AboutWorkflow.vue
@@ -0,0 +1,104 @@
+<script setup lang="ts">
+import { Pagination } from "swiper/modules";
+import 'swiper/css'
+import 'swiper/css/pagination'
+
+// import "swiper/swiper.min.css";
+// import "swiper/components/pagination/pagination.min.css"
+
+
+const workflowData = [
+  {
+    id: 1,
+    title: "Discover & Define",
+    teaser: "I start by understanding real user needs and frustrations.",
+  },
+  {
+    id: 2,
+    title: "Research & Inspiration",
+    teaser:
+      "I look into competitive products, design patterns, and real user pain points.",
+  },
+  {
+    id: 3,
+    title: "Plan & Scope",
+    teaser:
+      "I define the MVP, break the work into manageable pieces, and pick the right tech stack for the job.",
+  },
+  {
+    id: 4,
+    title: "Design & Architecture",
+    teaser:
+      "I build features that work well at the core and shine when supported.",
+  },
+  {
+    id: 5,
+    title: "Test & Polish",
+    teaser:
+      "I test across devices and browsers and perform accessibility checks.",
+  },
+  {
+    id: 6,
+    title: "Craft + Performance",
+    teaser: "It’s not done until it’s smooth, fast, and elegant.",
+  },
+  {
+    id: 7,
+    title: "Deploy & Reflect",
+    teaser:
+      "I ship early, monitor real-world usage, and reflect on what could be better. There’s always room to improve.",
+  },
+];
+
+</script>
+
+<template>
+  <div class="bg-dark py-20 relative">
+    <div class="container-fluid">
+      <h2 class="text-white text-4xl font-bold mb-5 w-full">My Workflow</h2>
+    </div>
+
+    <swiper-container
+      class="!overflow-visible custom-swiper"
+      style=""
+      :pagination="{
+         el: '.custom-swiper-progressbar',
+        type: 'progressbar',
+      }"
+      :modules="[Pagination]"
+      :breakpoints="{
+        0: {
+          spaceBetween: 24,
+          slidesPerView: 1.09,
+        },
+        480: {
+          spaceBetween: 24,
+          slidesPerView: 1.4,
+        },
+        768: {
+          spaceBetween: 16,
+          slidesPerView: 2.2,
+        },
+        1024: {
+          spaceBetween: 24,
+          slidesPerView: 2.7,
+        },
+      }"
+      :speed="1000"
+      :slides-offset-before="150"
+    >
+      <swiper-slide v-for="item in workflowData" :key="item?.id">
+          <workflowCard :data="item" :total-length="workflowData?.length"/>
+      </swiper-slide>
+    </swiper-container>
+
+    <div class="custom-swiper-progressbar absolute bottom-0 left-0 w-full h-1 bg-gray-300"></div>
+
+  </div>
+</template>
+
+<style>
+.custom-swiper-progressbar .swiper-pagination-progressbar-fill {
+  background-color: #3b82f6;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,6 +29,8 @@ export default defineNuxtConfig({
   },
   
   swiper: {
+    //@ts-ignore
+    modules: ["pagination"]
   },
 
   supabase: {
