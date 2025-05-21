18c017d9e85508f9efc5dfa68d1119bd11ef95e1
iagugunava <iagugunava05@gmail.com>
2025-05-21 14:47:41 +0400

    style: about page responsive

--- components/Skills.vue
+++ components/Skills.vue
@@ -8,7 +8,7 @@ const fetchSkills = async () => {
     const { data, error } = await supabaseClient.from('technologies').select('*');
 
     if(!error){
-        skills.value = data;
+        skills.value = data?.slice(0, 12);
     } else {
         console.log(error)
     }
@@ -23,7 +23,7 @@ fetchSkills()
     <div class="container-fluid py-20 bg-dark items-center flex flex-col gap-12 md:gap-8 justify-center">
         <h2 class="font-semibold text-2xl sm:text-4xl text-white w-fit">My Skills</h2>
 
-        <div class="grid gap-4 md:gap-6 grid-cols-3 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 w-full" >
+        <div class="grid gap-4 md:gap-6 grid-cols-3 sm:grid-cols-4 md:grid-cols-6 w-full" >
             <div v-for="item in skills" :key="item?.id">
                 <NuxtLink :to="item?.link ? item?.link : '#'" class="flex flex-col gap-2 items-center justify-center">
                     <div class="[&_svg]:w-8 [&_svg]:h-8 sm:[&_svg]:w-10 sm:[&_svg]:h-10" v-html="item?.image"></div>
